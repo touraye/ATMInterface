f0f2708cde12664a8d4a743e38aec1c8508b17e6
touraey <touraye07@gmail.com>
2021-07-01 15:16:09 +0100

    initial commit

---
+++ src/main/Login.java
@@ -0,0 +1,16 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("ATM INTERFACE");
+        primaryStage.show();
+    }
+
+    public static void main(String args[]){
+        launch(args);
+    }
+}
