d9473f508c4ddd96ae9d0a353b151eabfa5b9664
touraey <touraye07@gmail.com>
2021-07-06 08:57:39 +0100

    implement login

--- src/main/Login.java
+++ src/main/Login.java
@@ -1,27 +1,16 @@
 package main;
 
+import database.UserDB;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Login extends Application {
     private Statement sts;
     private Connection cn;
-    //Login
-    public Login(){
-       try {
-           cn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/atm", "root", "touraye7");
-           this.sts = cn.createStatement();
-           System.out.println("DB Connected");
-       } catch (SQLException ex){
-           System.out.println(ex.getMessage()); //log error msg
-           System.exit(0); //exit on failure
-       }
-    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("ATM INTERFACE");
@@ -29,6 +18,30 @@ public class Login extends Application {
     }
 
     public static void main(String args[]){
-        launch(args);
+        Scanner input = new Scanner(System.in);
+
+        String uID;
+        String uPin;
+
+        int counter = 0;
+        do{
+            System.out.println("Enter your user ID");
+            uID = input.next();
+            System.out.println("Enter your Pin");
+            uPin = input.next();
+            UserDB validate = new UserDB();//instantiate the UserDB class
+          boolean auth = validate.Login(uID, uPin);//making a method call and passing the params(uId, uPin)
+            if(auth){
+                launch(args);// launch the APP.
+                break;
+            }
+            System.out.println("Incorrect user name or pin. Please try again!");
+            counter++;
+            System.out.println(counter+ " out of 3 trail remain after that your card will Block!");
+        } while (counter < 3);
+
+        System.out.println("Card is Block!");//Block the ATM Card
+        System.exit(0);//Exit the APP.
+
     }
 }
