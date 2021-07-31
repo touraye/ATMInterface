7c05b62a44fd7915b521f85c3befe6b678eefd70
touraey <touraye07@gmail.com>
2021-07-31 10:42:57 +0100

    update alert

--- src/ui/Alert.java
+++ src/ui/Alert.java
@@ -3,29 +3,44 @@ package ui;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 
 public class Alert {
-    public static void display(String title, String Msg){
+    public static void display(String title,String info, double updatedBal, String New, double Msg){
         Stage window = new Stage();
 
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
-        window.setMinWidth(650);
+        window.setMinWidth(700);
         window.setMinHeight(250);
 
+        Label infoMsg = new Label();
+        infoMsg.setText(info);
+
         Label text = new Label();
-        text.setText(Msg);
+        text.setText(String.valueOf(Msg));
+
+        Label accountUpdate = new Label();
+        accountUpdate.setText(String.valueOf(updatedBal));
+
+        Label newDetails = new Label();
+        newDetails.setText(New);
+
         javafx.scene.control.Button closeBtn = new javafx.scene.control.Button("Close");
         closeBtn.setOnAction(event -> window.close());
 
         VBox layout = new VBox(30);
         layout.setAlignment(Pos.CENTER);
 
-        layout.getChildren().addAll(text, closeBtn);
+        HBox showMsg = new HBox(0);
+        showMsg.setAlignment(Pos.CENTER);
+        showMsg.getChildren().addAll(infoMsg, accountUpdate, newDetails, text);
+
+        layout.getChildren().addAll(showMsg, closeBtn);
 
         Scene scene = new Scene(layout);
         window.setScene(scene);
